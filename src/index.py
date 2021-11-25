b9cdfd04880a76d28851243fce05e1097f1f2595
Asta Björk <asta.bjork@helsinki.fi>
2021-11-25 17:53:23 +0200

    Limited UI in index.py for 1st sprint

--- src/index.py
+++ src/index.py
@@ -1,11 +1,12 @@
 def main():
-    print("LUKUVINKKIKIRJASTO")
+    print("\nLUKUVINKKIKIRJASTO")
     while True:
         print("\nValitse toiminto"
               "\n (1) lisää"
-              "\n (2) hae"
-              "\n (3) muokkaa"
-              "\n (4) poista"
+              "\n (2) listaa"
+            #   "\n (3) hae"
+            #   "\n (4) muokkaa"
+            #   "\n (5) poista"
               "\n (0) lopeta"
               )
 
@@ -14,11 +15,13 @@ def main():
         if vastaus.endswith("1"):
             print( "Lisätään lukuvinkki...")
         elif vastaus.endswith("2"):
-            print("Haetaan lukuvinkkiä...")
-        elif vastaus.endswith("3"):
-            print("Muokataan lukuvinkkiä...")
-        elif vastaus.endswith("4"):
-            print("Poistetaan lukuvinkki...")
+            print("Listataan lukuvinkit...")
+        # elif vastaus.endswith("3"):
+        #     print("Haetaan lukuvinkkiä...")
+        # elif vastaus.endswith("4"):
+        #     print("Muokataan lukuvinkkiä...")
+        # elif vastaus.endswith("5"):
+        #     print("Poistetaan lukuvinkki...")
         elif vastaus.endswith("0"):
             print("Heido!")
             break
