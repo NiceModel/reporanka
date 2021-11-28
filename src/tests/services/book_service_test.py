af062dfab3dc13af4507e13d9ece60f468d960d2
meri saynatkari <meri.saynatkari@gmail.com>
2021-11-28 22:23:03 +0200

    added all book service tests to one

--- src/tests/services/book_service_test.py
+++ src/tests/services/book_service_test.py
@@ -18,3 +18,20 @@ class TestBookRepository(unittest.TestCase):
     def test_find_all_books(self):
         books = self.book_service.find_all_books()
         self.assertTrue(isinstance(books, list))
+
+    def test_create_book_returns_correct_book(self):
+        book = self.book_service.create_book("Frank Herbert", "Dune", "1965")
+
+        self.assertEqual(book.author, "Frank Herbert")
+        self.assertEqual(book.title, "Dune")
+        self.assertEqual(book.published, "1965")
+
+    def test_find_all_books_returns_alphabetically(self):
+        self.book_service.create_book("Frank Herbert", "Dune", "1965")
+        self.book_service.create_book("Douglas Adams", "The Hitchhiker's Guide to the Galaxy", "1979")
+        self.book_service.create_book("J. R. R. Tolkien", "The Fellowship of the Ring", "1954")
+
+        books = self.book_service.find_all_books()
+        book_titles_alphabetic = ["Dune", "The Fellowship of the Ring", "The Hitchhiker's Guide to the Galaxy"]
+        for book, book_alphabetic in zip(books, book_titles_alphabetic):
+            self.assertEqual(book.title, book_alphabetic)
