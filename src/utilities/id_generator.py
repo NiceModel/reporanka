a653ce3803c9220807438a4655e7e5b5234d5e4e
NiceModel <olkinnun@gmail.com>
2021-12-09 18:28:45 +0200

    Create id_generator

---
+++ src/utilities/id_generator.py
@@ -0,0 +1,16 @@
+"""Id generator"""
+from repositories.item_repository import ITEM_REPOSITORY
+
+class IdGenerator:
+    """Generator for item ids"""
+    
+    def __init__(self):
+        self.id_number = len(ITEM_REPOSITORY.find_all())
+
+    def get_id(self):
+        """Updates id and returns new id"""
+        
+        self.id_number += 1
+        return self.id_number 
+
+ID_GENERATOR = IdGenerator()
