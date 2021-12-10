322d6c42a28ec0f0c54c6af45beea2823ec1cb36
meri saynatkari <meri.saynatkari@gmail.com>
2021-12-10 14:21:27 +0200

    moved command_factory to app; src/app excluded from coverage report (includes ui related modules that are tested with robot framework)

--- src/utilities/id_generator.py
+++ src/utilities/id_generator.py
@@ -3,13 +3,11 @@ from repositories.item_repository import ITEM_REPOSITORY
 
 class IdGenerator:
     """Generator for item ids"""
-    
     def __init__(self):
         self.id_number = max([int(item[0]) for item in ITEM_REPOSITORY.find_all()])
 
     def get_id(self):
         """Updates id and returns new id"""
-        
         self.id_number += 1
         return str(self.id_number).zfill(4)
 
