4f97dfdfe93e906367bea11669b06ee59c250907
meri saynatkari <meri.saynatkari@gmail.com>
2021-12-10 08:22:55 +0200

    changed entity test variables to existing books/blogs/videos; added tests for short string properties

--- src/tests/entities/video_test.py
+++ src/tests/entities/video_test.py
@@ -4,16 +4,27 @@ from entities.video import Video
 
 class TestVideo(unittest.TestCase):
     def setUp(self):
-        self.video = Video("sopuli", "video", "url", "1.1.2022")
+        self.video = Video(
+            "tglab", "Early mitotic divisions in a Drosophila embryo",
+            "https://youtu.be/XSKh-GLQn4E", "3.8.2008"
+            )
 
     def test_init_video(self):
         title = self.video.title
         address = self.video.address
         creator = self.video.creator
         published = self.video.published
-        self.assertEqual((title, address, creator, published), ("video", "url", "sopuli", "1.1.2022"))
+        self.assertEqual(
+            (title, address, creator, published),
+            ("Early mitotic divisions in a Drosophila embryo",
+             "https://youtu.be/XSKh-GLQn4E", "tglab", "3.8.2008")
+            )
 
     def test_video_string(self):
-        expected = "sopuli: video, url, (1.1.2022)"
+        expected = "tglab: Early mitotic divisions in a Drosophila embryo, https://youtu.be/XSKh-GLQn4E, (3.8.2008)"
         actual = str(self.video)
         self.assertEqual(actual, expected)
+
+    def test_video_short_string(self):
+        expected = 'tglab: Early mitotic divisions in a Drosophila embryo'
+        self.assertEqual(self.video.short_str, expected)
