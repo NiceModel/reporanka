2b03fa193c6de1056e284e3b2caa1203d77eae44
meri saynatkari <meri.saynatkari@gmail.com>
2021-12-11 15:06:01 +0200

    realised menu subclasses were not needed, on menu class works just as well

--- src/app/app.py
+++ src/app/app.py
@@ -1,20 +1,29 @@
 """Basic functionality"""
-
 # from utilities.utilities import check_year
 from config import TITLE
 # from app.command_factory import CommandFactory
-from app.menu import MainMenu, AddMenu
+from app.menu import Menu
 
 class App:
-    """Handles the UI functionality for the application"""
+    """Handles the UI functionality for the application
+    args:
+        io: handles the user inputs and app outputs
+        item_service: service for interacting with the item library
+
+    attr:
+        running: bool: True if app is currently running, otherwise false
+        menu: str: current menu to be displayed
+        main_menu: Menu: a menu object for the main menu
+        add_menu: Menu: a menu object for adding items
+    """
     def __init__(self, item_service, io):
         # self._item_service = item_service
         self._io = io
         # self._commands = CommandFactory(io, item_service)
         self.running = False
         self.menu = 'main'
-        self._main_menu = MainMenu(io, item_service)
-        self._add_menu = AddMenu(io, item_service)
+        self._main_menu = Menu(io, item_service)
+        self._add_menu = Menu(io, item_service, 'add')
 
     # def run(self):
     #     """Starts the application."""
@@ -26,6 +35,7 @@ class App:
     #         self.running = self.commands.get_command(command).perform()
 
     def run(self):
+        """Starts the application and runs until the user chooses to quit."""
         self.running = True
         self._io.write(TITLE)
         while self.running:
@@ -33,4 +43,3 @@ class App:
                 self.running, self.menu = self._main_menu.display()
             elif self.menu == 'add':
                 self.running, self.menu = self._add_menu.display()
-
--- src/app/menu.py
+++ src/app/menu.py
@@ -5,16 +5,51 @@ from app.actions import (
 )
 
 class Menu:
-    '''Superclass for menus.'''
-    def __init__(self, io, item_service):
+    '''Superclass for menus.
+    
+    args:
+        io: io for user inputs/console outputs
+        item_service: service for interacting with the item library
+        name: str: name of the menu; defaults to 'main'
+    attr:
+        cmds: dict: commands choosable from the menu
+        instructions: str: instructions for choosing menu commands
+        unknown: Action: placeholder action for unrecognised commands
+    '''
+    def __init__(self, io, item_service, name='main'):
         self._io = io
-        self._item_service = item_service
-        self._cmds = {}
-        self._instructions = ''
-        self._name = ''
+        self._name = name
         self._unknown = Action(io, item_service)
+        self._setup(item_service)
+
+    def _setup(self, item_service):
+        if self._name == 'main':
+            self._instructions = INSTRUCTIONS
+            self._cmds = {
+                "1": "add",
+                "2": List(self._io, item_service),
+                "3": Delete(self._io, item_service),
+                # "4": Search(self.io, self.item_service),
+                # "5": Modify(self.io, self.item_service),
+                "0": Quit(self._io, item_service)
+            }
+        elif self._name == 'add':
+            self._instructions = ADD_MENU
+            self._cmds = {
+                "1": AddBook(self._io, item_service),
+                "2": AddVideo(self._io, item_service),
+                "3": AddBlog(self._io, item_service),
+                "4": "main",
+                "0": Quit(self._io, item_service)
+            }
 
     def display(self):
+        '''Displays the current menu in console.
+
+        return:
+            running: bool: True if application is still running, otherwise false
+            menu: str: next menu to be displayed
+        '''
         running = True
         self._io.write(self._instructions)
         command = self._io.read(OUTPUTS['choice'])
@@ -28,39 +63,42 @@ class Menu:
         return running, menu
 
     def _get_action(self, command):
+        '''Internal method for fetching next executable command. Return unknown if
+        command is not found.
+        '''
         if command in self._cmds:
             return self._cmds[command]
-
         return self._unknown
 
     def _is_action(self, action):
+        '''Internal method for checking whether the action is Action or Menu.'''
         if isinstance(action, Action):
             return True
         return False
 
-class MainMenu(Menu):
-    def __init__(self, io, item_service):
-        super().__init__(io, item_service)
-        self._instructions = INSTRUCTIONS
-        self._name = 'main'
-        self._cmds = {
-            "1": "add",
-            "2": List(self._io, self._item_service),
-            "3": Delete(self._io, self._item_service),
-            # "4": Search(self.io, self.item_service),
-            # "5": Modify(self.io, self.item_service),
-            "0": Quit(self._io, self._item_service)
-        }
+# class MainMenu(Menu):
+#     def __init__(self, io, item_service):
+#         super().__init__(io, item_service)
+#         self._instructions = INSTRUCTIONS
+#         self._name = 'main'
+#         self._cmds = {
+#             "1": "add",
+#             "2": List(self._io, self._item_service),
+#             "3": Delete(self._io, self._item_service),
+#             # "4": Search(self.io, self.item_service),
+#             # "5": Modify(self.io, self.item_service),
+#             "0": Quit(self._io, self._item_service)
+#         }
 
-class AddMenu(Menu):
-    def __init__(self, io, item_service):
-        super().__init__(io, item_service)
-        self._instructions = ADD_MENU
-        self._name = 'add'
-        self._cmds = {
-            "1": AddBook(self._io, self._item_service),
-            "2": AddVideo(self._io, self._item_service),
-            "3": AddBlog(self._io, self._item_service),
-            "4": "main",
-            "0": Quit(self._io, self._item_service)
-        }
+# class AddMenu(Menu):
+#     def __init__(self, io, item_service):
+#         super().__init__(io, item_service)
+#         self._instructions = ADD_MENU
+#         self._name = 'add'
+#         self._cmds = {
+#             "1": AddBook(self._io, self._item_service),
+#             "2": AddVideo(self._io, self._item_service),
+#             "3": AddBlog(self._io, self._item_service),
+#             "4": "main",
+#             "0": Quit(self._io, self._item_service)
+#         }
